324beffcde3b70852b7f31f6e1487808ebb66da5
PawelDuplaga <pawelduplagait@gmail.com>
2023-01-11 18:45:23 +0100

    Firebase connection added

--- Breakfast/Services/Breakfast/BreakfastService.cs
+++ Breakfast/Services/Breakfast/BreakfastService.cs
@@ -9,16 +9,53 @@ namespace Breakfast.Services.Breakfast;
 
 public class BreakfastService : IBreakfastService
 {
-    private readonly Dictionary<Guid,BreakfastModel> _breakfasts = new Dictionary<Guid, BreakfastModel>();
+    IFirebaseConfig config;
+    IFirebaseClient client;
+     private static readonly Dictionary<Guid,BreakfastModel> _breakfasts = new Dictionary<Guid, BreakfastModel>();
+
+    public BreakfastService()
+    {
+        config = new FirebaseConfig
+        {
+            AuthSecret= "ojS09wi2FKqqj8QpmTgSvYrd0L6lfmDU2QUHtXKM", 
+            BasePath = "https://breakfastapi-6e85a-default-rtdb.europe-west1.firebasedatabase.app"
+        };
+    }
+
     public void CreateBreakfast(BreakfastModel breakfastModel)
     {
         _breakfasts.Add(breakfastModel.Id,breakfastModel);
+
+        try
+        {
+            client = new FireSharp.FirebaseClient(config);
+            var data = breakfastModel;
+            //PushResponse response = client.Push("Breakfasts/",data);
+            SetResponse setResponse = client.Set("Breakfasts/" + data.Id, data);
+
+            if(setResponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
+            }
+
+        }
+        catch (Exception ex)
+        {
+
+                Console.WriteLine(ex.Message);
+        }
+
     }
+
 
     public BreakfastModel GetBreakfast(Guid id)
     {
         return _breakfasts[id];
     }
-
-
 }
+
+
